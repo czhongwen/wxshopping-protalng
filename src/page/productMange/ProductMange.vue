c4e749eea6b3ee0ae8cb3e52cc843f9c1c4c5357
caozw <caozw@tuya.com>
2019-04-21 18:38:38 +0800

    增加商品管理

---
+++ src/page/productMange/ProductMange.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductMange',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
