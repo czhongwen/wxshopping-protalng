79cda80a94a591eb8864689aa6b28c62ebd66e60
caozw <caozw@tuya.com>
2019-04-23 00:12:01 +0800

    增加商品信息管理

--- src/page/productMange/ProductMange.vue
+++ src/page/productMange/ProductMange.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
     <!--头部导航-->
-    <div style="width: 100vw">
-      <el-cascader expand-trigger="hover" :options="primaryClassification" v-model="search.searchType" @change="handleChange">
+    <div style="width: 100vw;text-align: right;">
+      <el-cascader size="mini" expand-trigger="hover" :options="primaryClassification" v-model="search.searchType" @change="handleChange">
       </el-cascader>
+      <el-input v-bind="search.searchKey" placeholder="请输入关键字" size="mini" style="width: 10%;"></el-input>
+      <el-button size="mini" style="width: 8%;margin-right: 2%;" type="primary" icon="el-icon-search" @click="getProductList">搜索</el-button>
     </div>
 
     <!--表格信息-->
     <div>
-      <el-table :data="tableData" class="tableView">
-          <el-table-column prop="id" label="序号"></el-table-column>
-          <el-table-column prop="name" label="名称"></el-table-column>
-          <el-table-column prop="info" label="简介"></el-table-column>
-          <el-table-column prop="num" label="数量"></el-table-column>
-          <el-table-column prop="price" label="价格"></el-table-column>
-          <el-table-column prop="descount" label="折扣"></el-table-column>
+      <el-table :data="tableData" class="tableView" :header-cell-style="{'text-align':'center'}">
+        <el-table-column prop="id" label="序号"></el-table-column>
+        <el-table-column label="图片">
+          <template slot-scope="scope">
+            <img @click="productDetail(scope.row)" :src="'http://localhost:8081' + scope.row.image1" style="width: 50%;height: 50%;cursor:pointer"/>
+          </template>
+        </el-table-column>
+        <el-table-column prop="name" label="名称" style="overflow: hidden"></el-table-column>
+        <el-table-column prop="info" label="简介" style="overflow: hidden"></el-table-column>
+        <el-table-column prop="num" label="数量"></el-table-column>
+        <el-table-column prop="price" label="价格"></el-table-column>
+        <el-table-column prop="descount" label="折扣"></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button @click="obtained(scope.row)" size="mini" type="danger">下架</el-button>
+          </template>
+        </el-table-column>
       </el-table>
+      <el-pagination style="text-align: center" @size-change="handleSizeChange" @current-change="handleCurrentChange" :page-sizes="[10, 20, 30, 50]"
+        :page-size="page.limit"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="page.totalCount">
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -33,9 +50,9 @@ export default {
         searchKey: "",
       },
       page: {
-        pageSize: 18,
-        pageTotalCount: 0,
-        currentPage: 1,
+        limit: 10,
+        offset: 0,
+        totalCount: 1,
         order:"",
       },
       tableData:[],
@@ -82,15 +99,15 @@ export default {
       }
       this.$http.post("/api/product/getProductListV1", {
         typeId: typeId,
-        offset: _this.page.currentPage,
-        limit: _this.page.pageSize,
-        order: _this.page.order,
+        offset: _this.page.offset,
+        limit: _this.page.limit,
+        order: _this.page.order === "" ? null : _this.page.order,
       }).then((res) => {
         if (res.data.flag) {
           if (res.data.result.list.length > 0) {
             _this.tableData = [];
             _this.tableData = res.data.result.list;
-            console.log(_this.tableData)
+            _this.page.totalCount = res.data.result.count;
           } else {
             this.$message.error('对不起！未查询到信息！');
           }
@@ -99,9 +116,66 @@ export default {
         }
       })
     },
-    handleChange(value){
-      console.log(value);
+    /**
+     * 商品详情页
+     */
+    productDetail(row){
+      console.log(row.id);
+      this.$message.info("" + row.id);
     },
+
+    /**
+     * 选择分类
+     */
+    handleChange(value){
+      this.search.searchType = value;
+      this.page.offset = 0;
+      this.getProductList();
+    },
+    /**
+     * 修改每页的数量
+     */
+    handleSizeChange(val) {
+      this.page.limit = val;
+      this.page.offset = 0;
+      this.getProductList();
+    },
+
+    /**
+     * 分页当前页
+     */
+
+    handleCurrentChange(val) {
+      this.page.offset = (val - 1) * this.page.limit;
+      this.getProductList();
+    },
+
+    /**
+     * 下架商品
+     */
+    obtained(info){
+      let _this = this;
+      this.$confirm("确定要下架" + info.name + "这件商品吗？", "温馨提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+      }).then(() => {
+        _this.$http.post("/api/product/updateProduct", {
+          id: info.id,
+          status: 0,
+        }).then((res) => {
+          if (res.data.flag) {
+            if (res.data.result) {
+              _this.$message.success("下架成功!");
+              _this.getProductList();
+            } else {
+              _this.$message.error("下架失败！");
+            }
+          } else {
+            _this.$message.error(res.data.msg);
+          }
+        })
+      })
+    }
   },
   created () {
     this.getPrimaryClassification();
@@ -110,8 +184,14 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
   .tableView.el-table .cell{
     text-align: center!important;
+  }
+  .el-message-box {
+    text-align: center;
+  }
+  .el-message-box__btns{
+    text-align: center;
   }
 </style>
