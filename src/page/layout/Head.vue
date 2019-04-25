c55bec58f0ce9d0015cd4ae32f41bebf1d4c2f5f
caozw <caozw@tuya.com>
2019-04-25 20:35:27 +0800

    增加商品分类

--- src/page/layout/Head.vue
+++ src/page/layout/Head.vue
@@ -2,7 +2,7 @@
   <div style="border-radius: 3%;margin-bottom: 1%;">
     <el-menu :default-active="activeIndex" :router="true" @select="habl" active-text-color="#ffd04b" background-color="#545c64" class="el-menu-demo" mode="horizontal" style="border-radius: 3%" text-color="#fff">
       <el-menu-item index="/page/productMange/ProductMange">商品管理</el-menu-item>
-      <el-menu-item index="">购物车管理</el-menu-item>
+      <el-menu-item index="1">购物车管理</el-menu-item>
       <el-menu-item index="3">地址管理</el-menu-item>
       <el-menu-item index="4">订单管理</el-menu-item>
       <el-menu-item index="/page/productType/ProductTypeMange">商品分类管理</el-menu-item>
@@ -15,12 +15,12 @@
       name: "Head",
       data(){
           return {
-            activeIndex: "/page/productMange/ProductMange",
+            activeIndex: "",
           }
       },
       methods: {
         habl(key, path){
-          console.log(key, path);
+          this.activeIndex = path;
         }
       }
     }
--- src/page/productType/ProductTypeMange.vue
+++ src/page/productType/ProductTypeMange.vue
@@ -1,15 +1,83 @@
 <template>
   <div>
-    加油
+    <el-table :data="tableData">
+      <el-table-column label="序号" prop="typeId"></el-table-column>
+      <el-table-column label="图片">
+        <template slot-scope="scope">
+          <img :src="'http://localhost:8081' + scope.row.typeImage" @click="productDetail(scope.row)" style="width: 50%;height: 50%;cursor:pointer"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="分类名称" prop="typename"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button @click="updateProductType(scope.row)" size="mini" type="success">修改</el-button>
+          <el-button @click="" size="mini" type="success">详情</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog :visible.sync="productTypeVisible" center title="修改一级分类" width="30%">
+      <span style="line-height: 100%">
+        <label style="line-height: 50%">图片</label>
+        <img :src="'http://localhost:8081' + typeImage" style="width: 50%;height: 50%;"/>
+      </span>
+      <span class="dialog-footer" slot="footer">
+        <el-button @click="productTypeVisible = false"  size="mini">取 消</el-button>
+        <el-button @click="productTypeVisible = false" size="mini" type="primary">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
     export default {
-        name: "ProductTypeMange"
+      name: "ProductTypeMange",
+      data(){
+          return{
+            tableData:[],
+            productTypeVisible:false,
+            id:0,
+            typeName:"",
+            typeImage: "",
+          }
+      },
+      methods:{
+        getTypeList(){
+          let _this = this;
+          this.$http.post("/api/productType/getAllType").then((res) => {
+            console.log(res);
+            if (res.data.flag) {
+              if (res.data.result.length > 0) {
+                _this.tableData = []
+                _this.tableData = res.data.result
+              } else {
+                _this.$message.error("数据为空!");
+              }
+            } else {
+              _this.$message.error(res.data.msg);
+            }
+          })
+        },
+        updateProductType(info) {
+          let _this = this;
+          _this.typeName = info.typename;
+          _this.typeImage = info.typeImage;
+          _this.productTypeVisible = true;
+        },
+      },
+      created() {
+        this.getTypeList();
+      }
     }
 </script>
 
-<style scoped>
-
+<style>
+  .el-table .cell{
+    text-align: center!important;
+  }
+  .el-message-box {
+    text-align: center;
+  }
+  .el-message-box__btns{
+    text-align: center;
+  }
 </style>
