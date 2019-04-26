03e82f70529d9aabd2d1b00b37786bb4908a5c0e
caozw <caozw@tuya.com>
2019-04-26 15:42:04 +0800

    修改页面样式

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,11 @@ export default {
 
 <style>
 #app {
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
+  color: #2c3e50;
+  margin-top: 10px;
 }
 </style>
